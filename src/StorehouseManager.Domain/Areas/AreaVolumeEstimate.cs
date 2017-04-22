0d546abea9b7ce0264a2194ce540e063f6bd4da5
kostya9 <kostya9@gmail.com>
2017-04-22 16:56:51 +0300

    Adding area marking reports

---
+++ src/StorehouseManager.Domain/Areas/AreaVolumeEstimate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StorehouseManager.Domain.Goods;
+
+namespace StorehouseManager.Domain.Areas
+{
+    public class AreaVolumeEstimate
+    {
+        private readonly GoodsRepository _goodsRepository;
+
+        public AreaVolumeEstimate(GoodsRepository goodsRepository)
+        {
+            _goodsRepository = goodsRepository;
+        }
+
+        public double Calculate(Area area)
+        {
+            return _goodsRepository.GoodsItems.Where(gi => gi.AreaId == area.Id).Sum(gi => gi.Characteristics.Volume);
+        }
+    }
+}
